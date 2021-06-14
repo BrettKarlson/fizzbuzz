509bf03f3331eebc280c0d3e8fc99cb2077a43cc
Brett Karlson <brettjkarlson@gmail.com>
2021-06-14 16:30:23 +0100

    updated fizzbuzz and fizzbuzz_spec files to include al functionality

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(number)
-  if number == 15
+  if number % 15 == 0
     return 'fizzbuzz'
   elsif number % 3 == 0
     return 'fizz'
-  else
+  elsif number % 5 == 0
     return 'buzz'
+  else
+    number
   end
 end
